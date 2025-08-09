827e0204eb70bc906da361beb862fda3b6992c51
jishnupd13 <jishnupd13@gmail.com>
2025-08-10 00:11:07 +0530

    display a sample quote in DailyQuoteScreen

--- app/src/main/java/com/app/activitiesapplication/ui/daily_quote/DailyQuote.kt
+++ app/src/main/java/com/app/activitiesapplication/ui/daily_quote/DailyQuote.kt
@@ -2,12 +2,18 @@ package com.app.activitiesapplication.ui.daily_quote
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DailyQuoteScreen() {
@@ -16,7 +22,21 @@ fun DailyQuoteScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Daily Quotes Screen")
+        Text(
+            text = "Do or Die",
+            fontSize = 32.sp,
+            fontStyle = FontStyle.Italic
+        )
+
+        Spacer(modifier = Modifier.size(14.dp))
+
+        Text(
+            text = "Mahatma Gandhi",
+            fontSize = 16.sp,
+            fontStyle = FontStyle.Italic,
+            color = Color.Gray
+        )
+
     }
 }
 
