debc17922e9e8de8b11428b5669afd2e03a478b6
jishnupd13 <jishnupd13@gmail.com>
2025-08-11 16:25:36 +0530

    bugs in quotes app completed

--- app/src/main/java/com/app/quotes_daily/ui/daily_quote/DailyQuote.kt
+++ app/src/main/java/com/app/quotes_daily/ui/daily_quote/DailyQuote.kt
@@ -1,10 +1,16 @@
 package com.app.quotes_daily.ui.daily_quote
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,18 +19,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.app.quotes_daily.viewmodel.daily_quote.DailyQuoteViewModel
+import kotlin.math.max
 
 @Composable
 fun DailyQuoteScreen(
     viewModel: DailyQuoteViewModel = hiltViewModel()
 ) {
+
     val state by viewModel.uiState.collectAsStateWithLifecycle()
+    val scrollState = rememberScrollState()
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -40,23 +52,64 @@ fun DailyQuoteScreen(
             }
             is DailyQuoteViewModel.DailyQuoteUiState.Success -> {
                 val data = state as DailyQuoteViewModel.DailyQuoteUiState.Success
-                Text(
-                    text = data.quote,
-                    fontSize = 32.sp,
-                    fontStyle = FontStyle.Italic
-                )
-
-                Spacer(modifier = Modifier.size(14.dp))
-
-                Text(
-                    text = data.author,
-                    fontSize = 16.sp,
-                    fontStyle = FontStyle.Italic,
-                    color = Color.Gray
+                QuotesTile(
+                    quote = data.quote,
+                    author = data.author,
+                    scrollState = scrollState
                 )
             }
         }
     }
+}
+
+@Composable
+fun QuotesTile(
+    quote: String,
+    author: String,
+    scrollState: ScrollState
+){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize()
+            .verticalScroll(scrollState)
+    ) {
+        Text(
+            text = quote,
+            fontSize = 32.sp,
+            fontStyle = FontStyle.Italic,
+            lineHeight = 1.5.em,
+            modifier = Modifier
+                .padding(horizontal = 14.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
+
+        Spacer(modifier = Modifier.size(14.dp))
+
+        Text(
+            text = author,
+            fontSize = 16.sp,
+            fontStyle = FontStyle.Italic,
+            color = Color.Gray,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 14.dp),
+            textAlign = TextAlign.End,
+            maxLines = 2
+
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun QuotesTilePreview(){
+    QuotesTile(
+        quote = "Do or Die",
+        author = "Franklin D. Roosevelt",
+        scrollState = rememberScrollState()
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
